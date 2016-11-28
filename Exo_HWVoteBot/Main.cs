69db2b62ea995100a4096d7b0f321782dac45c2f
Exorion <jason.rivillon@gmail.com>
2016-11-28 11:37:54 +0100

    Well needed code cleaning.

--- Exo_HWVoteBot/Main.cs
+++ Exo_HWVoteBot/Main.cs
@@ -10,10 +10,16 @@ namespace Exo_HWVoteBot
 {
 	public partial class Main : Form
 	{
+		#region Functions import
+
 		[DllImport("wininet.dll", CharSet = CharSet.Auto, SetLastError = true)]
 		public static extern bool InternetSetCookie(string lpszUrlName, string lbszCookieName, string lpszCookieData);
 		[DllImport("Kernel32.dll")]
 		static extern int GetLastError();
+
+		#endregion
+
+		#region Global Variables
 
 		Version version = new Version("1.2");
 		RegistryKey windowsStartup = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
@@ -24,60 +30,62 @@ namespace Exo_HWVoteBot
 		bool voting = false;
 		int currentSite = 0;
 
+		#endregion
+
 		public Main()
 		{
 			InitializeComponent();
+			CheckIfWSIsEnabled();
+		}
 
+		#region Windows Startup
+
+		private void CheckIfWSIsEnabled()
+		{
 			if (windowsStartup.GetValue("EHWVB /minimized /enabled") == null)
 				CB_WindowsStartup.Checked = false;
 			else
 				CB_WindowsStartup.Checked = true;
 		}
+		private void CB_WindowsStartup_CheckedChanged(object sender, EventArgs e)
+		{
+			if (CB_WindowsStartup.Checked)
+				windowsStartup.SetValue("EHWVB /minimized /enabled", Application.ExecutablePath);
+			else
+				windowsStartup.DeleteValue("EHWVB /minimized /enabled", false);
+		}
+
+		#endregion
+
+		#region Loading
 
 		private void Main_Shown(object sender, EventArgs e)
 		{
 			LBL_Status.Text = "Loading...";
 
 			((Control)WB_Main).Enabled = false;
+			string[] args = Environment.GetCommandLineArgs();
+			foreach (string arg in args)
+			{
+				switch (arg)
+				{
+					case "/minimized":
+						NTI_Main.Visible = true;
+						WindowState = FormWindowState.Minimized;
+						Hide();
+						break;
+				}
+			}
 
 			LBL_Status.Text = "Checking for internet connection...";
 			if (CheckInternetConnection("http://www.google.com"))
 			{
-				LBL_Status.Text = "Checking for new version...";
-				WebClient versionDownloader = new WebClient();
-				versionDownloader.DownloadFile("https://raw.githubusercontent.com/ExoKalork/EHWVB/master/version.txt", @"\version");
-				StreamReader reader = new StreamReader(@"\version");
-				if (Version.Parse(reader.ReadLine()) > version)
-				{
-					DialogResult dialogResult = MessageBox.Show("New version available ! Do you want to download it ?", "EHWVB", MessageBoxButtons.YesNo);
-					if (dialogResult == DialogResult.Yes)
-					{
-						reader.Close();
-						File.Delete(@"\version");
-						Process.Start("https://github.com/ExoKalork/EHWVB/releases");
-						Environment.Exit(0);
-					}
-				}
-				reader.Close();
-				File.Delete(@"\version");
+				CheckForNewVersion();
 
 				LBL_Status.Text = "Checking for heroes-wow.com availability...";
 				if (CheckInternetConnection("http://heroes-wow.com/wotlk/"))
 				{
 					ConnectCheck();
-
-					string[] args = Environment.GetCommandLineArgs();
-					foreach (string arg in args)
-					{
-						switch (arg)
-						{
-							case "/minimized":
-								NTI_Main.Visible = true;
-								WindowState = FormWindowState.Minimized;
-								Hide();
-								break;
-						}
-					}
 				}
 				else
 				{
@@ -92,164 +100,105 @@ namespace Exo_HWVoteBot
 			}
 		}
 
-		private void ConnectCheck()
+		#endregion
+
+		#region Version Checking
+
+		private void CheckForNewVersion()
 		{
-			connectCheck = true;
-			WB_Main.Navigate("https://heroes-wow.com/wotlk/index.php?page=login");
-			LBL_Status.Text = "Checking if user is online...";
+			LBL_Status.Text = "Checking for new version...";
+			WebClient versionDownloader = new WebClient();
+			versionDownloader.DownloadFile("https://raw.githubusercontent.com/ExoKalork/EHWVB/master/version.txt", "EHWVBVersion.txt");
+			StreamReader reader = new StreamReader("EHWVBVersion.txt");
+			if (Version.Parse(reader.ReadLine()) > version)
+			{
+				DialogResult dialogResult = MessageBox.Show("New version available ! Do you want to download it ?", "EHWVB", MessageBoxButtons.YesNo);
+				if (dialogResult == DialogResult.Yes)
+				{
+					reader.Close();
+					File.Delete("EHWVBVersion.txt");
+					Process.Start("https://github.com/ExoKalork/EHWVB/releases");
+					Environment.Exit(0);
+				}
+			}
+			reader.Close();
+			File.Delete("EHWVBVersion.txt");
 		}
+
+		#endregion
 
 		private void WB_Main_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
 		{
 			WB_Main.Stop();
-			if (disconnecting)
-			{
-				if (WB_Main.Url.ToString() == "https://heroes-wow.com/")
-					WB_Main.Navigate("https://heroes-wow.com/wotlk/logout.php");
-				else if (WB_Main.Url.ToString() == "https://heroes-wow.com/wotlk/")
-				{
-					disconnecting = false;
-					MessageBox.Show("Done !");
-					Environment.Exit(0);
-				}
-			}
-			else if (connectCheck)
-			{
-				if (!needConnect)
-				{
-					if (WB_Main.Url.ToString() == "https://heroes-wow.com/wotlk/index.php?page=login")
-						TM_ConnectedCheck.Start();
-					else if (WB_Main.Url.ToString() == "https://heroes-wow.com/wotlk/index.php" && TM_ConnectedCheck.Enabled)
-					{
-						connectCheck = false;
-						TM_ConnectedCheck.Stop();
-						BT_EnableDisable.Enabled = true;
-						LBL_Status.Text = "Ready !";
-						if (voting)
-							Vote(true);
-						else
-							if (Array.Exists(Environment.GetCommandLineArgs(), arg => arg == "/enabled"))
-								EnableDisable();
-					}
-				}
-				else if (needConnect && WB_Main.Url.ToString() == "https://heroes-wow.com/wotlk/index.php?page=loginb")
-				{
-					connectCheck = false;
-					MessageBox.Show("Connection successfull !");
-					needConnect = false;
-					((Control)WB_Main).Enabled = false;
-					BT_EnableDisable.Enabled = true;
-					LBL_Status.Text = "Ready !";
-					if (voting)
-						Vote(true);
-					else
-						if (Array.Exists(Environment.GetCommandLineArgs(), arg => arg == "/enabled"))
-							EnableDisable();
-				}
-			}
-			else
-			{
-				if (WB_Main.Url.ToString() == "https://heroes-wow.com/wotlk/index.php?page=vote")
-				{
-					LBL_Status.Text = "Voted on site " + currentSite;
-					switch (currentSite)
-					{
-						case 1:
-							currentSite++;
-							InternetSetCookie("http://heroes-wow.com", "HTTP_REFERER", "http%3A%2F%2Fwww.top100arena.com%2Fout.asp%3Fid%3D44752");
-							WB_Main.Navigate("https://heroes-wow.com/wotlk/execute.php?take=vote&site=2");
-							break;
-						case 2:
-							currentSite++;
-							InternetSetCookie("http://heroes-wow.com", "HTTP_REFERER", "http%3A%2F%2Fwww.openwow.com%2Fvisit%3D2125");
-							WB_Main.Navigate("https://heroes-wow.com/wotlk/execute.php?take=vote&site=3");
-							break;
-						case 3:
-							currentSite++;
-							InternetSetCookie("http://heroes-wow.com", "HTTP_REFERER", "http%3A%2F%2Fwww.rpg-paradize.com%2Fsite-Heroes%2BWoW%2B548%2Band%2B335a%2B255%2BLevel-22237");
-							WB_Main.Navigate("https://heroes-wow.com/wotlk/execute.php?take=vote&site=4");
-							break;
-						case 4:
-							currentSite++;
-							InternetSetCookie("http://heroes-wow.com", "HTTP_REFERER", "http%3A%2F%2Ftopg.org%2Fserver-heroes-wow-id347987");
-							WB_Main.Navigate("https://heroes-wow.com/wotlk/execute.php?take=vote&site=5");
-							break;
-						case 5:
-							currentSite++;
-							InternetSetCookie("http://heroes-wow.com", "HTTP_REFERER", "http%3A%2F%2Fwww.wowstatus.net%2F%7Ewowstatus%2Fserverlist%2Fout%2Fid%2F725206%2Flink%2Fhomepage");
-							WB_Main.Navigate("https://heroes-wow.com/wotlk/execute.php?take=vote&site=6");
-							break;
-						case 6:
-							currentSite += 2;
-							InternetSetCookie("http://heroes-wow.com", "HTTP_REFERER", "http%3A%2F%2Ftopwow.ru%2Findex.php");
-							WB_Main.Navigate("https://heroes-wow.com/wotlk/execute.php?take=vote&site=8");
-							break;
-						case 8:
-							currentSite += 2;
-							InternetSetCookie("http://heroes-wow.com", "HTTP_REFERER", "http%3A%2F%2Fwww.wowtop.es%2Findex.php");
-							WB_Main.Navigate("https://heroes-wow.com/wotlk/execute.php?take=vote&site=10");
-							break;
-						case 10:
-							currentSite++;
-							InternetSetCookie("http://heroes-wow.com", "HTTP_REFERER", "http%3A%2F%2Fwww.gowonda.com%2Fserveur-wow-4956-Heroes-WoW-5.4.8-and-3.3.5a-255-Level.htm");
-							WB_Main.Navigate("https://heroes-wow.com/wotlk/execute.php?take=vote&site=11");
-							break;
-						case 11:
-							LBL_Status.Text = "Successfully voted !";
-							voting = false;
-							currentSite = 0;
-							if (!File.Exists("LastVote"))
-								File.Create("LastVote").Close();
-							StreamWriter writer = new StreamWriter("LastVote");
-							writer.Write(DateTime.Now);
-							writer.Close();
-							TM_VoteCheck.Start();
-							WB_Main.Navigate("http://heroes-wow.com/wotlk/");
-							break;
-					}
-				}
-			}
+			Disconnect();
+			Connect();
+			VoteProcess();
 		}
 
-		private void TM_ConnectedCheck_Tick(object sender, EventArgs e)
+		#region VoteProcess
+
+		private void VoteProcess()
 		{
-			TM_ConnectedCheck.Stop();
-			if (WindowState == FormWindowState.Minimized)
+			if (WB_Main.Url.ToString() == "https://heroes-wow.com/wotlk/index.php?page=vote")
 			{
-				NTI_Main.Visible = false;
-				Show();
-				WindowState = FormWindowState.Normal;
-			}
-			MessageBox.Show("Please connect to Heroes-WoW's website.");
-			LBL_Status.Text = "Waiting for connection...";
-			needConnect = true;
-			((Control)WB_Main).Enabled = true;
-			WB_Main.Navigate("https://heroes-wow.com/wotlk/index.php?page=login");
-		}
-
-		private bool CheckInternetConnection(string url)
-		{
-			try
-			{
-				using (var client = new WebClient())
+				LBL_Status.Text = "Voted on site " + currentSite;
+				switch (currentSite)
 				{
-					using (var stream = client.OpenRead(url))
-					{
-						return true;
-					}
+					case 1:
+						currentSite++;
+						InternetSetCookie("http://heroes-wow.com", "HTTP_REFERER", "http%3A%2F%2Fwww.top100arena.com%2Fout.asp%3Fid%3D44752");
+						WB_Main.Navigate("https://heroes-wow.com/wotlk/execute.php?take=vote&site=2");
+						break;
+					case 2:
+						currentSite++;
+						InternetSetCookie("http://heroes-wow.com", "HTTP_REFERER", "http%3A%2F%2Fwww.openwow.com%2Fvisit%3D2125");
+						WB_Main.Navigate("https://heroes-wow.com/wotlk/execute.php?take=vote&site=3");
+						break;
+					case 3:
+						currentSite++;
+						InternetSetCookie("http://heroes-wow.com", "HTTP_REFERER", "http%3A%2F%2Fwww.rpg-paradize.com%2Fsite-Heroes%2BWoW%2B548%2Band%2B335a%2B255%2BLevel-22237");
+						WB_Main.Navigate("https://heroes-wow.com/wotlk/execute.php?take=vote&site=4");
+						break;
+					case 4:
+						currentSite++;
+						InternetSetCookie("http://heroes-wow.com", "HTTP_REFERER", "http%3A%2F%2Ftopg.org%2Fserver-heroes-wow-id347987");
+						WB_Main.Navigate("https://heroes-wow.com/wotlk/execute.php?take=vote&site=5");
+						break;
+					case 5:
+						currentSite++;
+						InternetSetCookie("http://heroes-wow.com", "HTTP_REFERER", "http%3A%2F%2Fwww.wowstatus.net%2F%7Ewowstatus%2Fserverlist%2Fout%2Fid%2F725206%2Flink%2Fhomepage");
+						WB_Main.Navigate("https://heroes-wow.com/wotlk/execute.php?take=vote&site=6");
+						break;
+					case 6:
+						currentSite += 2;
+						InternetSetCookie("http://heroes-wow.com", "HTTP_REFERER", "http%3A%2F%2Ftopwow.ru%2Findex.php");
+						WB_Main.Navigate("https://heroes-wow.com/wotlk/execute.php?take=vote&site=8");
+						break;
+					case 8:
+						currentSite += 2;
+						InternetSetCookie("http://heroes-wow.com", "HTTP_REFERER", "http%3A%2F%2Fwww.wowtop.es%2Findex.php");
+						WB_Main.Navigate("https://heroes-wow.com/wotlk/execute.php?take=vote&site=10");
+						break;
+					case 10:
+						currentSite++;
+						InternetSetCookie("http://heroes-wow.com", "HTTP_REFERER", "http%3A%2F%2Fwww.gowonda.com%2Fserveur-wow-4956-Heroes-WoW-5.4.8-and-3.3.5a-255-Level.htm");
+						WB_Main.Navigate("https://heroes-wow.com/wotlk/execute.php?take=vote&site=11");
+						break;
+					case 11:
+						LBL_Status.Text = "Successfully voted !";
+						voting = false;
+						currentSite = 0;
+						if (!File.Exists("LastVote"))
+							File.Create("LastVote").Close();
+						StreamWriter writer = new StreamWriter("LastVote");
+						writer.Write(DateTime.Now);
+						writer.Close();
+						TM_VoteCheck.Start();
+						WB_Main.Navigate("http://heroes-wow.com/wotlk/");
+						break;
 				}
 			}
-			catch
-			{
-				return false;
-			}
 		}
-
-		private void BT_EnableDisable_Click(object sender, EventArgs e)
-		{
-			EnableDisable();
-		}
-
 		private void EnableDisable()
 		{
 			if (!enabled)
@@ -270,21 +219,10 @@ namespace Exo_HWVoteBot
 				TM_VoteCheck.Stop();
 			}
 		}
-
-		private void disconnectMeToolStripMenuItem_Click(object sender, EventArgs e)
+		private void BT_EnableDisable_Click(object sender, EventArgs e)
 		{
-			if (!enabled)
-			{
-				disconnecting = true;
-				WB_Main.Navigate("https://heroes-wow.com/");
-			}
+			EnableDisable();
 		}
-
-		private void qUitToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			Environment.Exit(0);
-		}
-
 		private void TM_VoteCheck_Tick(object sender, EventArgs e)
 		{
 			TM_VoteCheck.Stop();
@@ -315,7 +253,6 @@ namespace Exo_HWVoteBot
 			else
 				Vote(false);
 		}
-
 		private void Vote(bool online)
 		{
 			voting = true;
@@ -333,6 +270,100 @@ namespace Exo_HWVoteBot
 			}
 		}
 
+		#endregion
+
+		#region Connect
+
+		private void Connect()
+		{
+			if (connectCheck)
+			{
+				if (!needConnect)
+				{
+					if (WB_Main.Url.ToString() == "https://heroes-wow.com/wotlk/index.php?page=login")
+						TM_ConnectedCheck.Start();
+					else if (WB_Main.Url.ToString() == "https://heroes-wow.com/wotlk/index.php" && TM_ConnectedCheck.Enabled)
+					{
+						connectCheck = false;
+						TM_ConnectedCheck.Stop();
+						BT_EnableDisable.Enabled = true;
+						LBL_Status.Text = "Ready !";
+						if (voting)
+							Vote(true);
+						else
+							if (Array.Exists(Environment.GetCommandLineArgs(), arg => arg == "/enabled"))
+							EnableDisable();
+					}
+				}
+				else if (needConnect && WB_Main.Url.ToString() == "https://heroes-wow.com/wotlk/index.php?page=loginb")
+				{
+					connectCheck = false;
+					MessageBox.Show("Connection successfull !");
+					needConnect = false;
+					((Control)WB_Main).Enabled = false;
+					BT_EnableDisable.Enabled = true;
+					LBL_Status.Text = "Ready !";
+					if (voting)
+						Vote(true);
+					else
+						if (Array.Exists(Environment.GetCommandLineArgs(), arg => arg == "/enabled"))
+						EnableDisable();
+				}
+			}
+		}
+		private void ConnectCheck()
+		{
+			connectCheck = true;
+			WB_Main.Navigate("https://heroes-wow.com/wotlk/index.php?page=login");
+			LBL_Status.Text = "Checking if user is online...";
+		}
+		private void TM_ConnectedCheck_Tick(object sender, EventArgs e)
+		{
+			TM_ConnectedCheck.Stop();
+			if (WindowState == FormWindowState.Minimized)
+			{
+				NTI_Main.Visible = false;
+				Show();
+				WindowState = FormWindowState.Normal;
+			}
+			MessageBox.Show("Please connect to Heroes-WoW's website.");
+			LBL_Status.Text = "Waiting for connection...";
+			needConnect = true;
+			((Control)WB_Main).Enabled = true;
+			WB_Main.Navigate("https://heroes-wow.com/wotlk/index.php?page=login");
+		}
+
+		#endregion
+
+		#region Disconnect
+
+		private void Disconnect()
+		{
+			if (disconnecting)
+			{
+				if (WB_Main.Url.ToString() == "https://heroes-wow.com/")
+					WB_Main.Navigate("https://heroes-wow.com/wotlk/logout.php");
+				else if (WB_Main.Url.ToString() == "https://heroes-wow.com/wotlk/")
+				{
+					disconnecting = false;
+					MessageBox.Show("Done !");
+					Environment.Exit(0);
+				}
+			}
+		}
+		private void disconnectMeToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			if (!enabled)
+			{
+				disconnecting = true;
+				WB_Main.Navigate("https://heroes-wow.com/");
+			}
+		}
+
+		#endregion
+
+		#region User Control
+
 		private void Main_Resize(object sender, EventArgs e)
 		{
 			if (WindowState == FormWindowState.Minimized)
@@ -342,22 +373,16 @@ namespace Exo_HWVoteBot
 				Hide();
 			}
 		}
-
 		private void NTI_Main_MouseDoubleClick(object sender, MouseEventArgs e)
 		{
 			NTI_Main.Visible = false;
 			Show();
 			WindowState = FormWindowState.Normal;
 		}
-
-		private void CB_WindowsStartup_CheckedChanged(object sender, EventArgs e)
+		private void qUitToolStripMenuItem_Click(object sender, EventArgs e)
 		{
-			if (CB_WindowsStartup.Checked)
-				windowsStartup.SetValue("EHWVB /minimized /enabled", Application.ExecutablePath);
-			else
-				windowsStartup.DeleteValue("EHWVB /minimized /enabled", false);
+			Environment.Exit(0);
 		}
-
 		private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
 		{
 			if (!voting && !needConnect)
@@ -365,5 +390,29 @@ namespace Exo_HWVoteBot
 			else
 				MessageBox.Show("Please wait until your current task is done.");
 		}
+
+		#endregion
+
+		#region Tools
+
+		private bool CheckInternetConnection(string url)
+		{
+			try
+			{
+				using (var client = new WebClient())
+				{
+					using (var stream = client.OpenRead(url))
+					{
+						return true;
+					}
+				}
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		#endregion
 	}
 }
